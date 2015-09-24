b6812cd5c5c4b5f8d238830d53210aac477b508e
amstut_a <arthur.amstutz@gmail.com>
2015-09-24 18:07:41 +0200

    add @at_end

--- J5.py
+++ J5.py
@@ -11,10 +11,9 @@ class MaPremiereClasse(Grammar, Declaration):
 
     entry = "translation_unit"
     grammar = """
-    declaration = [Declaration.declaration | beg]
-    
+    declaration = [Declaration.declaration | beg | end]
     beg = ["@begin" '(' id :i ')' Statement.compound_statement :st #add_begin(current_block, i, st)]
-    
+    end = ["@end" '(' id :i ')' Statement.compound_statement :st #add_end(current_block, i, st)]
     """
 
 
@@ -25,14 +24,28 @@ class AtBegin:
         self.body = body
         self.idx = idx
 
+
+class AtEnd:
+    def __init__(self, ast, fname, body, idx):
+        self.ast = ast
+        self.fname = fname
+        self.body = body
+        self.idx = idx
+
+
         
 @meta.hook(MaPremiereClasse)
 def add_begin(self, block, idi, statement):
-
     topDoggyDog = AtBegin(block.ref, self.value(idi), statement.body, len(block.ref.body))
     block.ref.body.append(topDoggyDog)
     return True
 
+
+@meta.hook(MaPremiereClasse)
+def add_end(self, block, idi, statement):
+    topDoggyDog = AtEnd(block.ref, self.value(idi), statement.body, len(block.ref.body))
+    block.ref.body.append(topDoggyDog)
+    return True
 
 
 def maSuperTransfo(ast):
@@ -41,10 +54,20 @@ def maSuperTransfo(ast):
         if isinstance(it, AtBegin):
             maSuperListe.append(it)
             for i in it.ast.body:
-                if not isinstance(i, AtBegin):
+                if not isinstance(i, AtBegin) and not isinstance(i, AtEnd):
                     if i._name == it.fname:
                         for idx, val in enumerate(it.body):
                             i.body.body.insert(idx, val)
+
+
+        if isinstance(it, AtEnd):
+            maSuperListe.append(it)
+            for i in it.ast.body:
+                if not isinstance(i, AtBegin) and not isinstance(i, AtEnd):
+                    if i._name == it.fname:
+                        for val in it.body:
+                            i.body.body.append(val)
+                            
     for it in reversed(maSuperListe):
         ast.body.pop(it.idx)
 
