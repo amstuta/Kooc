974fc444f0ba4a51e404117546f76143c723388f
amstut_a <arthur.amstutz@gmail.com>
2015-10-01 15:37:25 +0200

    rec des modules & implementations

--- module.py
+++ module.py
@@ -1,10 +1,23 @@
+from mangler import *
+
 class Module:
     
-    def __init__(self, ident, statement):
+    def __init__(self, ident, statement, flag):
+
+        # Declarations
         self.decls = {}
-        self.ident = ident
         for st in statement.body:
-            self.decls[Mangler.instance().muckFangle(st)] = st
+            self.decls[Mangler.instance().muckFangle(ident, st)] = st
+
+        # Nom du module
+        self.ident = ident
+
+        # Tout ce qui doit etre mis dans le fichier de sortie (extern, ...)
+        self.out = []
+
+        # Declaration trouvee dans le fichier courrant ou non
+        self.current_file = flag
+
 
     def __getitem__(self, idx):
         if idx in self.decls:
