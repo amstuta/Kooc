37393d50ddfcecae938f51f503c43d788fa6ec04
amstut_a <arthur.amstutz@gmail.com>
2015-10-01 21:17:23 +0200

    gestion BlockStmt dans add_implementation

--- implementation.py
+++ implementation.py
@@ -7,9 +7,15 @@ class Implementation:
 
         self.imps = {}
         for i in imp.body:
-            dec_i = Mangler.instance().muckFangle(i, ident)
-            i._name = dec_i
-            self.imps[dec_i] = i
+            if isinstance(i, cnorm.nodes.BlockStmt):
+                for elem in i.body:
+                    dec_elem = Mangler.instance().muckFangle(elem, ident)
+                    elem._name = dec_elem
+                    self.imps[dec_elem] = elem
+            else:
+                dec_i = Mangler.instance().muckFangle(i, ident)
+                i._name = dec_i
+                self.imps[dec_i] = i
 
 
     def __getitem__(self, ident):
