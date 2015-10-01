723506f37c4fb25b5bed153f3177d06dfcb4669f
elkaim_r <thecatisaspy@gmail.com>
2015-10-01 17:36:30 +0200

    ajout proto du mangler

--- mangler.py
+++ mangler.py
@@ -1,7 +1,32 @@
+
+import cnorm
+
 class Mangler:
     
-    def __init__(self, symbols):
-        self.symbols = symbols
-
-    def mangle(self):
+    def __init__(self):
         pass
+
+    inst = None
+
+    @staticmethod
+    def instance():
+        if (Mangler.inst == None):
+            Mangler.inst = Mangler()
+
+        return Mangler.inst
+
+    def muckFangle(self, proto, module):
+        res = ""
+        if type(proto._ctype) == cnorm.nodes.FuncType :
+            res += "Func"
+        else:
+            res += "Var"
+        res += '$'
+        res += module + '$'
+        res += proto._name + '$'
+        res += '$' + proto._ctype._identifier
+        if type(proto._ctype) == cnorm.nodes.FuncType:
+            for par in proto._ctype.params:
+                res += '$' + par._ctype._identifier
+
+        return res
