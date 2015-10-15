0acfe1f9dea8becb04fd6255e1bff1961c81658f
amstut_a <arthur.amstutz@gmail.com>
2015-10-15 18:12:26 +0200

    unit test pour mangler complet

--- unittests/ut_mangler/test_mangler.py
+++ unittests/ut_mangler/test_mangler.py
@@ -8,18 +8,28 @@ from mangler import *
 
 class ManglerTestCase(unittest.TestCase):
         def setUp(self):
-            self.mangler = Mangler.instance()
+                self.mangler = Mangler.instance()
 
         def tearDown(self):
-            self.mangler = None
+                self.mangler = None
 
         def test_complete_mangle(self):
-            ex_decl = cnorm.nodes.Decl('i', cnorm.nodes.PrimaryType('int'))
-            print(self.mangler.muckFangle(ex_decl, 'A'))
-            self.assertEqual(self.mangler.muckFangle(ex_decl, 'A'), 'Var$B$i$int', 'incorrect complete mangling')
+                print('Test Mangling complet')
+                ex_decl = cnorm.nodes.Decl('i', cnorm.nodes.PrimaryType('int'))
+                self.assertEqual(self.mangler.muckFangle(ex_decl, 'A'), 'Var$A$i$$int', 'incorrect complete mangling')
+
+        def test_simple_mangle(self):
+                print('Test Mangling simple')
+
+                # void clean(Object*);
+                decl_clean = cnorm.nodes.Decl('clean', cnorm.nodes.PrimaryType(''))
+                setattr(decl_clean._ctype, '_decltype', cnorm.nodes.PointerType())
+                setattr(decl_clean._ctype._decltype, '_decltype', cnorm.nodes.ParenType([cnorm.nodes.Decl('', cnorm.nodes.PrimaryType('Object'))]))
+                setattr(decl_clean._ctype._decltype._decltype._params[0]._ctype, '_decltype', cnorm.nodes.PointerType())
+                self.assertEqual(self.mangler.mimpleSangle(decl_clean), 'clean$P$', 'incorrect simple mangling')
 
 
-# Renvoit la fct de test
-test = ManglerTestCase('test_complete_mangle')
-testSuite = unittest.TestSuite()
-testSuite.addTest(test)
+
+
+if __name__ == '__main__':
+        unittest.main()
