3053d59b2e9035bbeaef8b19035feeda4405100d
amstut_a <arthur.amstutz@gmail.com>
2015-10-19 11:50:26 +0200

    move classe kooc dans kooc_class.py

--- unittests/ut_module/test_module.py
+++ unittests/ut_module/test_module.py
@@ -4,6 +4,7 @@ import sys
 sys.path.insert(0,'../../')
 import unittest
 import cnorm
+from kooc_class import *
 
 class ModuleTestCase(unittest.TestCase):
     def setUp(self):
@@ -11,6 +12,25 @@ class ModuleTestCase(unittest.TestCase):
 
     def tearDown(self):
         pass
+
+    def test_module_simple(self):
+        decl_i = cnorm.nodes.Decl('Var$A$i$$int', cnorm.nodes.PrimaryType('int'))
+        setattr(decl_i, '_assign_expr', cnorm.nodes.Literal('1'))
+        decl_f = cnorm.nodes.Decl('Func$A$f$$void', cnorm.nodes.FuncType('void', []))
+
+        i_found = False
+        f_found = False
+        
+        a = Kooc()
+        res = a.parse_file('./ex_mod_1.kh')
+        for elem in res.body:
+            if elem == decl_i:
+                i_found = True
+            elif elem == decl_f:
+                f_found = True
+
+        self.assertTrue(i_found)
+        self.assertTrue(f_found)
 
 
 if __name__ == '__main__':
